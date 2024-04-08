5351dca4f5150728edd34cb1cb0940a8e7b64518
lixinyi <xinyi.li@transwarp.io>
2024-04-08 16:49:34 +0800

    feat: 登陆页面开发完毕

--- src/views/Login/LoginPanel/LoginForm/index.tsx
+++ src/views/Login/LoginPanel/LoginForm/index.tsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Segmented, ConfigProvider, Form, Input, Select, Button } from 'antd';
 import './index.less';
+import { useNavigate } from 'react-router-dom';
 
+const validatePhone = () => [
+  {
+    required: true, 
+    message: '请输入你的手机号',
+  },
+  {
+    message: '请输入正确的手机号',
+    pattern:new RegExp(/^1(3|4|5|6|7|8|9)\d{9}$/, "g"),
+  }
+]
 function LoginForm() {
   const [role, setRole] = useState<string>('student');
+  const [countdown, setCountdown] = useState<number>(0);
+  const navigate = useNavigate();
 
   // Form配置
   const formItemLayout = {
@@ -29,10 +42,27 @@ function LoginForm() {
     try {
       await form.validateFields();
       const values = form.validateFields();
+      console.log('values', values);
+      navigate('..');
     } catch(_) {
-
+      return;
     }
   }
+
+  // 点击“发送验证码”后逻辑
+  const sendVericode = () => {
+    if (countdown === 0) {
+      setCountdown(60);
+    }
+  }
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCountdown(prev => prev - 1);
+    }, 1000)
+    if(countdown === 0) {
+      clearTimeout(timer);
+    }
+  }, [countdown])
 
   return (
     <div className='relative' style={{height: '100%', width: '50%', float: 'left'}}>
@@ -70,20 +100,55 @@ function LoginForm() {
                 scrollToFirstError
                 wrapperCol={{span: 24}}
               >
-                <Form.Item
-                  name="phone"
-                  rules={[{ required: true, message: '请输入你的手机号' }]}
-                  style={{marginBottom: '28px'}}
-                >
-                  <Input addonBefore={prefixSelector} placeholder="手机号" style={{ width: '100%' }}/>
-                </Form.Item>
-                <Form.Item
-                  name="verificationCode"
-                  rules={[{ required: true, message: '请输入验证码' }]}
-                  style={{marginBottom: '30px'}}
-                >
-                  <Input suffix={<Button type='link' style={{color: '#8c68a5', fontSize: '16px', height: '35px'}}>发送验证码</Button>} placeholder="短信验证码" style={{ width: '100%' }}/>
-                </Form.Item>
+                {
+                  role == 'administrator' ?
+                  <>
+                    <Form.Item
+                      name="account"
+                      rules={[{ required: true, message: '请输入账号' }]}
+                      style={{marginBottom: '28px'}}
+                    >
+                      <Input placeholder="账号" style={{ width: '100%' }}/>
+                    </Form.Item> 
+                    <Form.Item
+                      name="password"
+                      rules={[{ required: true, message: '请输入密码' }]}
+                      style={{marginBottom: '30px'}}
+                    >
+                      <Input
+                        placeholder="密码" 
+                        style={{ width: '100%', height: 49 }}
+                      />
+                    </Form.Item>
+                  </> :
+                  <>
+                    <Form.Item
+                      name="phone"
+                      rules={[
+                        ...validatePhone(),
+                      ]}
+                      style={{marginBottom: '28px'}}
+                    >
+                      <Input addonBefore={prefixSelector} placeholder="手机号" style={{ width: '100%' }}/>
+                    </Form.Item> 
+                    <Form.Item
+                      name="verificationCode"
+                      rules={[{ required: true, message: '请输入验证码' }]}
+                      style={{marginBottom: '30px'}}
+                    >
+                      <Input 
+                        suffix={
+                          countdown === 0 ?
+                          <Button type='link' style={{color: '#8c68a5', fontSize: '16px', height: '35px'}} onClick={sendVericode}>发送验证码</Button> :
+                          <Button type='link' disabled style={{fontSize: '16px'}}>{`已发送${countdown}s`}</Button>
+                        }
+                        placeholder="短信验证码" 
+                        style={{ width: '100%' }}
+                        autoComplete='off'
+                      />
+                    </Form.Item>
+                  </>
+                }
               </Form>
               <Button type='text' onClick={handleSubmit}>登录/注册</Button>
             </ConfigProvider>
